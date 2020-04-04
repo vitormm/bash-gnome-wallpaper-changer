cfcf116cbe8dedc17ec3596657024394634c8f18
Vitor Matta <vitormm@gmail.com>
2020-04-04 13:37:17 -0300

    Use other galleries with beautiful pics

--- changeWallpaper.sh
+++ changeWallpaper.sh
@@ -6,6 +6,10 @@
 # Email: vitormm@gmail.com                                                          #
 #####################################################################################
 
-wget https://source.unsplash.com/random/1920x1080/?wallpaper -O /tmp/wallpaper_temp.jpeg
+photoGalleries=("nasa" "textures-patterns" "wallpapers" "nature" "architecture" "animals" "travel")
+randomIndex=$(awk -v min=0 -v max=6 -v seed="$(od -An -N4 -tu4 /dev/urandom)" 'BEGIN{srand(seed+0); print int(min+rand()*(max-min+1))}')
+randomGallery=${photoGalleries[randomIndex]}
+
+wget https://source.unsplash.com/random/1920x1080/?${randomGallery} -O /tmp/wallpaper_temp.jpeg
 mv /tmp/wallpaper_temp.jpeg /tmp/wallpaper.jpeg
 gsettings set org.gnome.desktop.background picture-uri "file:///tmp/wallpaper.jpeg"
